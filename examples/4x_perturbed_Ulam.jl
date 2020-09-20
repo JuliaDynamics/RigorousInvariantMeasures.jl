fe08df2f8b2018e79336f134df34646dc8c54fde
Isaia Nisoli <nisoli@im.ufrj.br>
2020-09-20 14:13:16 -0300

    Added the files

---
+++ examples/4x_perturbed_Ulam.jl
@@ -0,0 +1,39 @@
+using InvariantMeasures
+using ValidatedNumerics
+
+m = 15
+m_extend = 100
+
+D = Mod1Dynamic(x -> 4*x + 0.01*InvariantMeasures.sinpi(8*x))
+B = Ulam(1024)
+Q = DiscretizedOperator(B, D)
+
+normQ = opnormbound(weak_norm(B), Q)
+
+trivial_norms = norms_of_powers_trivial(weak_norm(B), Q, m)
+computed_norms = norms_of_powers(weak_norm(B), m, Q.L, true)
+
+(dfly_strongs, dfly_norms) = norms_of_powers_dfly(B, D, m)
+
+norms = min.(trivial_norms, computed_norms, dfly_norms) # in the current version, dfly_norms are always larger and can be omitted
+
+better_norms = refine_norms_of_powers(norms, m_extend)
+
+w = invariant_vector(B, Q)
+@show distance_from_invariant(B, D, Q, w, better_norms)
+
+B_fine = Ulam(2^16)
+Q_fine = DiscretizedOperator(B_fine, D)
+norm_Q_fine = opnormbound(weak_norm(B_fine), Q_fine)
+
+trivial_norms_fine = norms_of_powers_trivial(weak_norm(B_fine), Q_fine, m_extend)
+twogrid_norms_fine = norms_of_powers_from_coarser_grid(B_fine, B, D, better_norms, norm_Q_fine)
+
+(dfly_strongs_fine, dfly_norms_fine) = norms_of_powers_dfly(B_fine, D, m_extend)
+
+norms_fine = min.(trivial_norms_fine, twogrid_norms_fine, dfly_norms_fine)
+
+better_norms_fine = refine_norms_of_powers(norms_fine, m_extend)
+
+w_fine = invariant_vector(B_fine, Q_fine)
+@show distance_from_invariant(B_fine, D, Q_fine, w_fine, better_norms_fine)
