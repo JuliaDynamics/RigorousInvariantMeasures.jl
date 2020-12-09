3880f9bee0020210d36ae0c27a79cd227d6a586b
Federico Poloni <fpoloni@di.unipi.it>
2020-12-09 15:42:48 +0100

    comments

--- src/PwDynamicDefinition.jl
+++ src/PwDynamicDefinition.jl
@@ -4,6 +4,11 @@ using ..DynamicDefinition, ..Contractors
 
 export PwMap, preim, nbranches, plottable
 
+"""
+Dynamic based on a piecewise defined map.
+
+The map is defined as T(x) = Ts[k](x) if x ∈ [endpoints[k], endpoints[k+1])
+"""
 struct PwMap <: Dynamic
 	Ts::Array{Function, 1}
 	endpoints::Array{Interval, 1}
@@ -17,16 +22,13 @@ DynamicDefinition.is_full_branch(D::PwMap) = false
 import DualNumbers: Dual
 PwMap(Ts, endpoints) = PwMap(Ts, endpoints, [sign(Ts[i](Dual((endpoints[i]+endpoints[i+1])/2, 1)).epsilon) for i in 1:length(endpoints)-1])
 
-
-
 function DynamicDefinition.preim(D::PwMap, k, y, ϵ)
-	# we need to treat the case with the other orientation, 0 not fixed point...
 	@assert 1 <= k <= nbranches(D)
 	domain = hull(D.endpoints[k], D.endpoints[k+1])
 	root(x->D.Ts[k](x)-y, domain, ϵ)
 end
 
-function DynamicDefinition.plottable(D::PwMap, x) 
+function DynamicDefinition.plottable(D::PwMap, x)
 	for k in nbranches(D)
 		domain = hull(D.endpoints[k], D.endpoints[k+1])
 		if x in domain
@@ -36,5 +38,3 @@ function DynamicDefinition.plottable(D::PwMap, x)
 end
 
 end
-
-
