e9cea5baf16ef16382145fa5ca052b5dc7f570f7
Isaia Nisoli <nisoli@im.ufrj.br>
2022-07-23 14:09:39 +0900

    Restructuring following TaylorSeries.jl

--- docs/make.jl
+++ docs/make.jl
@@ -4,7 +4,13 @@ makedocs(sitename="RigorousInvariantMeasures.jl",
     format = Documenter.HTML(
         prettyurls = get(ENV, "CI", nothing) == "true"
     ),
-    modules = [RigorousInvariantMeasures]
+    pages=[
+        "Home" => "index.md",
+        "Background" => "background.md",
+        "User guide" => "userguide.md",
+        "Examples" => "examples.md",
+        "API" => "api.md"
+    ],
 )
 
 deploydocs(
