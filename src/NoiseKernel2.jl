983938d195c4a08a9d0a7b625c25f864709ebd59
Isaia Nisoli <nisoli@im.ufrj.br>
2022-11-29 11:26:12 +0900

    Uniform noise running on CPU, all test

--- src/NoiseKernel2.jl
+++ src/NoiseKernel2.jl
@@ -3,11 +3,15 @@ using LinearAlgebra
 struct NoiseUlam
     B::Ulam
     k::Integer
-    BC
-    Ext
+    BC::Matrix
+    Ext::Matrix
     w_ext::Vector
     w_conv::Vector
+    ξ 
 end
+
+NoiseUlam(B, k, BC, Ext) = NoiseUlam(B, k, BC, Ext, zeros(length(B)+k-1), zeros(length(B)+k-1), Float64(k)/length(B))
+
 
 """
     Extension_Operator
@@ -83,6 +87,7 @@ with k odd
 """
 function UniformNoiseUlam2(B::Ulam, k; boundary_condition = :periodic)
     @assert k%2 == 1
+    l = (k-1) ÷ 2
 
     n = length(B) 
 
@@ -90,40 +95,48 @@ function UniformNoiseUlam2(B::Ulam, k; boundary_condition = :periodic)
     w_conv = zeros(n+2*k)
 
     if boundary_condition == :periodic 
-        BC = PeriodicBoundaryConditionOperator(n, k)
+        BC = PeriodicBoundaryConditionOperator(n, l)
     elseif boundary_condition == :reflecting
-        BC = PeriodicBoundaryConditionOperator(n, k)
+        BC = PeriodicBoundaryConditionOperator(n, l)
     else
         @error "Not implemented"
     end
-    Ext = ExtensionOperator(n, k)
+    Ext = ExtensionOperator(n, l)
     
-    return NoiseUlam(B, k, BC, Ext, w_ext, w_conv) 
+    return NoiseUlam(B, k, BC, Ext) 
 end
 
 import LinearAlgebra: mul!
 
-function LinearAlgebra.mul!(w::Vector{Float64}, N::NoiseUlam, v::Vector{Float64})
-    @info typeof(N)
-
-    mul!(N.w_ext, N.Ext, v)
-
-    n = length(N.B)
-    k = N.k
-    # # we assume it is extended by zero
-    for i in 1:k
-        N.w_conv[i] = sum(N.w_ext[i:i+k])
+function uniform_convolution!(w_conv, w_ext, l)
+    n = length(w_ext)-2*l
+    
+    for i in 1:l
+        w_conv[i] = sum(@view w_ext[i:i+l])
     end 
 
-    for i in 1:length(v)
-        N.w_conv[i+k] = sum(N.w_ext[i:i+2*k]) 
+    for i in 1:n
+        w_conv[i+l] = sum(@view w_ext[i:i+2*l]) 
     end
 
-    for i in 1:k
-        N.w_conv[n+k+i] = sum(N.w_ext[n+i-k:n+i])
+    for i in 1:l
+        w_conv[n+l+i] = sum(@view w_ext[n+i:n+l+i])
     end 
+    w_conv ./= (2*l+1)
+end
 
-    N.w_conv ./= k
+function LinearAlgebra.mul!(w::Vector{Float64}, N::NoiseUlam, v::Vector{Float64})
+    mul!(N.w_ext, N.Ext, v)
+    k = N.k
+    l = (k-1) ÷ 2
+
+    uniform_convolution!(N.w_conv, N.w_ext, l)
 
     mul!(w, N.BC, N.w_conv)
 end 
+
+function Base.:*(N::NoiseUlam, v::Vector{Float64})
+    w = zeros(length(v))
+    mul!(w, N, v)
+    return w
+end
--- test/TestNoise2.jl
+++ test/TestNoise2.jl
@@ -3,7 +3,8 @@
     import RigorousInvariantMeasures: ExtensionOperator, 
                                       PeriodicBoundaryConditionOperator, 
                                       ReflectingBoundaryConditionOperator,
-                                      UniformNoiseUlam2
+                                      UniformNoiseUlam2,
+                                      uniform_convolution!
 
     using LinearAlgebra
     
@@ -18,6 +19,13 @@
     BC = PeriodicBoundaryConditionOperator(256, 16)
     @test all(Per[:, 256-16+1:512+16].== BC)
 
+    v = zeros(256+16*2)
+
+    v[256+16+1] = 1 
+    w = BC * v
+
+    @test w[1] == 1
+
     BC = PeriodicBoundaryConditionOperator(256, 128)
     @test all(Per[:, 256-128+1:512+128].== BC)
 
@@ -27,13 +35,39 @@
     RC = ReflectingBoundaryConditionOperator(256, 16)
     @test all(Ref[:, 256-16+1:512+16].== RC)
 
+    v = zeros(256+16*2)
+    v[256+16+1] = 1 
+    w = RC * v
+    @test w[end] == 1
+
     RC = ReflectingBoundaryConditionOperator(256, 128)
     @test all(Ref[:, 256-128+1:512+128].== RC)
 
-    N = UniformNoiseUlam(Ulam(32), 5)
+    l = 9
+    w_ext = zeros(1042)
+    w_ext[10] = 1
+    w_conv = zeros(1042)
+    uniform_convolution!(w_conv, w_ext, l)
 
-    
+    @test all(w_conv[1:19] .== Float64(1)/19)  
 
+    w_ext = zeros(1042)
+    w_ext[1024+l] = 1
+    w_conv = zeros(1042)
+    uniform_convolution!(w_conv, w_ext, l)
+    @test all(w_conv[1024:end] .== Float64(1)/19)  
+
+    n = 1024
+    B = Ulam(n)
+
+    k = 19
+    l = (k-1)÷2
+    N = UniformNoiseUlam2(B, k)
+
+    v = zeros(length(B))
+    v[1] = 1
+    w = N*v
+    @test all(w[1:l].==1/k) && all(w[end-l+1:end].== 1/k)
 
 
 end
